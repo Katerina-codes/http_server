7f150765723c5ac00f6ad0974a581ecf42c6bb1c
Katerina Georgiou <katerinaldg@gmail.com>
2018-07-19 06:44:06 +0100

    Server has a host.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,11 @@
 public class Server {
 
+    public String host;
+
+    public Server(String host) {
+        this.host = host;
+    }
+
     public void run(ServerSocketManager socketManager) {
         socketManager.accept();
     }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,16 +1,27 @@
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ServerTest {
 
     @Test
     public void testsAConnectionCanBeMade() {
-        Server server = new Server();
+        Server server = new Server("localhost");
         ServerSocketSpy serverSocketSpy = new ServerSocketSpy();
 
         server.run(serverSocketSpy);
 
         assertTrue(serverSocketSpy.acceptWasCalled);
+    }
+
+    @Test
+    public void serverHasAHost() {
+        Server server = new Server("localhost");
+
+        assertEquals("localhost", server.host);
     }
 }
