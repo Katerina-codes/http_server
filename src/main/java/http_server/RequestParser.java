1738006cc8a9f1a61f4025c023a93ac821d2da41
Katerina Georgiou <katerinaldg@gmail.com>
2018-08-03 11:00:25 +0100

    Extract method to remove duplication.

--- src/main/java/http_server/RequestParser.java
+++ src/main/java/http_server/RequestParser.java
@@ -2,22 +2,26 @@ package http_server;
 
 import java.util.List;
 
+import static http_server.ResponseMaker.TEXT;
 import static java.util.Arrays.asList;
 
 public class RequestParser {
 
+    public static final int FROM_STARTING_INDEX = 0;
+
     public String parseResource(String request) {
-        int startOfString = request.indexOf("/", 0) + 1;
-        String substring = request.substring(startOfString, request.lastIndexOf(" "));
+        String rootDirectory = "/";
+        int startOfString = getStartOfString(request, rootDirectory, FROM_STARTING_INDEX);
+        String substring = getSubstring(request, startOfString, " ");
         if (substring.equals("")) {
-            return "/";
+            return rootDirectory;
         } else {
             return substring;
         }
     }
 
     public HttpMethods extractMethodFromRequest(String request) {
-        String methodFromRequest = request.substring(0, request.lastIndexOf(" /"));
+        String methodFromRequest = getSubstring(request, FROM_STARTING_INDEX, " /");
         HttpMethods convertedMethod = null;
         List<HttpMethods> methods = asList(HttpMethods.values());
         for (HttpMethods method : methods) {
@@ -30,11 +34,20 @@ public class RequestParser {
 
     public String parseContentType(String resource) {
         if (resource.equals("file1") || resource.equals("file2")) {
-            return "txt";
+            return TEXT;
         } else {
-            int startOfString = resource.indexOf(".", 0) + 1;
+            int startOfString = getStartOfString(resource, ".", FROM_STARTING_INDEX);
             return resource.substring(startOfString, resource.length());
         }
+    }
+
+    private int getStartOfString(String resource, String fromCharacter, int fromIndex) {
+        int addOneToIndex = 1;
+        return resource.indexOf(fromCharacter, fromIndex) + addOneToIndex;
+    }
+
+    private String getSubstring(String request, int startOfString, String endIndex) {
+        return request.substring(startOfString, request.lastIndexOf(endIndex));
     }
 
 }
