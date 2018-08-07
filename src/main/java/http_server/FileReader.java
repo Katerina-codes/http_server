a1c4b917f799a7fd38598e68a7553a681d92d50f
Katerina Georgiou <katerinaldg@gmail.com>
2018-08-07 21:44:44 +0100

    Returns all files from directory.

--- src/main/java/http_server/FileReader.java
+++ src/main/java/http_server/FileReader.java
@@ -1,9 +1,15 @@
 package http_server;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.*;
 
 public class FileReader {
 
@@ -19,6 +25,41 @@ public class FileReader {
             e.printStackTrace();
         }
         return contents;
+    }
+
+    public List<String> returnDirectoryContents(String path) throws DirectoryNotFoundException {
+        Path directoryPath = Paths.get(path);
+        if (Files.exists(directoryPath)) {
+            List<File> files = getFiles(directoryPath);
+            return extractFileNames(files);
+        } else throw new DirectoryNotFoundException();
+    }
+
+    private List<File> getFiles(Path directoryPath) {
+        File directory = new File(directoryPath.toString());
+        File[] listOfFiles = directory.listFiles();
+        List<File> files;
+        files = directoryContents(listOfFiles);
+        return files;
+    }
+
+    private List<File> directoryContents(File[] listOfFiles) {
+        List<File> files;
+        if (listOfFiles != null) {
+            files = asList(listOfFiles);
+        } else {
+            files = emptyList();
+        }
+        return files;
+    }
+
+    private ArrayList<String> extractFileNames(List<File> files) {
+        ArrayList<String> extractedFileNames = new ArrayList<>();
+        for (File file : files) {
+            String fileName = file.toString().substring(7, file.toString().length());
+            extractedFileNames.add(fileName);
+        }
+        return extractedFileNames;
     }
 
 }
--- src/test/java/http_server/FileReaderTest.java
+++ src/test/java/http_server/FileReaderTest.java
@@ -3,6 +3,11 @@ package http_server;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
 public class FileReaderTest {
 
     @Test
@@ -10,6 +15,22 @@ public class FileReaderTest {
         FileReader fileReader = new FileReader();
 
         Assert.assertArrayEquals("file1 contents".getBytes(), fileReader.returnResourceContents("file1"));
+    }
+
+    @Test
+    public void returnAllFiles() throws DirectoryNotFoundException {
+        FileReader fileReader = new FileReader();
+
+        List<String> directoryContents = asList("file1",
+                "file2",
+                "image.gif",
+                "image.jpeg",
+                "image.png",
+                "partial_content.txt",
+                "patch-content.txt",
+                "text-file.txt");
+
+        assertEquals(directoryContents, fileReader.returnDirectoryContents("public/"));
     }
 
 }
